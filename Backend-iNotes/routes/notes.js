3569821441bf635de12ad81c3bb0c00546ec2039
Mohd Uvaish <81020580+uvaishnasir@users.noreply.github.com>
2023-09-25 00:22:57 +0530

    updated note api

--- Backend-iNotes/routes/notes.js
+++ Backend-iNotes/routes/notes.js
@@ -33,18 +33,39 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { title, description, tag } = req.body;
-
-    const note = new Notes({
-      title: title,
-      description: description,
-      tag: tag,
-      user: req.user.id,
-    });
-
-    const savedNote = await note.save();
-    res.json(savedNote);
+    try {
+      const { title, description, tag } = req.body;
+      const note = new Notes({ title, description, tag, user: req.user.id });
+      const savedNote = await note.save();
+      res.json(savedNote);
+    } catch (e) {
+      console.error(e.message);
+      res.status(500).send("Internal Server Error");
+    }
   }
 );
+
+// Route 3
+//update note using -PUT-- "api/notes/updatenote". login required.
+
+router.put("/updatenote/:id", fetchUser, async (req, res) => {
+
+  const {title, description, tag} = req.body;
+  //create the new dummy note.
+  const newNote = {};
+  if(title){newNote.title = title}
+  if(description){newNote.description = description}
+  if(tag){newNote.tag = tag}
+
+  //find the note to be updated and update it.
+  let note = await Notes.findById(req.params.id);
+
+  if(!note) return res.status(404).send("Note doesn't exist");
+
+  //check the user and update note.
+  if(note.user.toString()!== req.user.id) return res.status(401).send("Not Allowed");
+  note = await Notes.findByIdAndUpdate(req.params.id, {$set:newNote},{new:true});
+  res.json(note);
+});
 
 module.exports = router;
