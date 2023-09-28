4583d73b51ad44e482f5dc1f993e5b240da907be
Mohd Uvaish <81020580+uvaishnasir@users.noreply.github.com>
2023-09-28 23:33:29 +0530

    Navbar and routes componenets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
